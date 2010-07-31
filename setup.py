a54241cbf86281b1dca12dc7420fb0b88a87b4f2
Jonathan Kinred <jonathan.kinred@gmail.com>
2010-07-31 16:07:45 +1000

    Basic configuration for building project

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='psphere',
+      version='0.1',
+      description='vSphere SDK for Python',
+      author='Jonathan Kinred',
+      author_email='jonathan.kinred@gmail.com',
+      url='http://jkinred.bitbucket.org/psphere',
+      packages=['psphere'],
+      classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+      ],
+     )
