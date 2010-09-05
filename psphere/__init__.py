c28b4c31e905e6a77f9406b6e839b98f139e98f7
Jonathan Kinred <jonathan.kinred@gmail.com>
2010-09-05 10:17:09 +1000

    Use the hg version so the website displays that it's n development

--- psphere/__init__.py
+++ psphere/__init__.py
@@ -12,3 +12,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+__version__ = '0.1'
+__released__ = '0.1 (hg)'
