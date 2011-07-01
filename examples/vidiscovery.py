8bdee80992768553fcbe2e2046b48ee6fb8861f1
Jonathan Kinred <jonathan.kinred@gmail.com>
2011-07-01 16:05:13 +1000

    Convert script to next generation

--- examples/vidiscovery.py
+++ examples/vidiscovery.py
@@ -16,8 +16,9 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from psphere.scripting import BaseScript
+from psphere.managedobjects import ClusterComputeResource
 
-class VimDiscovery(BaseScript):
+class Discovery(BaseScript):
     def discovery(self, url=None, username=None, password=None):
         """An example that discovers hosts and VMs in the inventory.
 
@@ -33,23 +34,19 @@ class VimDiscovery(BaseScript):
         """
         self.login()
         # Find the first ClusterComputeResource
-        ccs = self.vim.find_entity_view(view_type='ClusterComputeResource',
-                                filter={'name': 'Application Engineering'},
-                                        properties=['name', 'host'])
+        ccs = ClusterComputeResource.find_one(
+                                    filter={'name': 'Application Engineering'},
+                                    properties=['name', 'host'])
         print('Cluster: %s (%s hosts)' % (ccs.name, len(ccs.host)))
 
-        # Get the host views in one fell swoop
-        hosts = self.vim.get_views(mo_refs=ccs.host,
-                                   properties=['name', 'vm'])
-        for host in hosts:
+        for host in ccs.hosts:
             print('  Host: %s (%s VMs)' % (host.name, len(host.vm)))
             # Get the vm views in one fell swoop
-            vms = self.vim.get_views(mo_refs=host.vm, properties=['name'])
-            for vm in vms:
+            for vm in host.vm:
                 print('    VM: %s' % vm.name)
     
 def main():
-    vd = VimDiscovery()
+    vd = Discovery()
     vd.discovery()
 
 if __name__ == '__main__':
