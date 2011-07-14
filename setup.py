a9364d9ed861f37108491baec6c6ed2cfd20def8
Jonathan Kinred <jonathan.kinred@gmail.com>
2011-07-14 23:57:22 +1000

    Bump version to 0.1.2. Add keywords

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@
 from setuptools import setup
 
 setup(name='psphere',
-      version='0.1.1',
+      version='0.1.2',
       description='vSphere SDK for Python',
       author='Jonathan Kinred',
       author_email='jonathan.kinred@gmail.com',
       url='http://bitbucket.org/jkinred/psphere',
       packages=['psphere'],
       install_requires=['suds'],
+      keywords=['vsphere', 'vmware'],
       classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "License :: OSI Approved :: Apache Software License",
