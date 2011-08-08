0add3648e0875d597cfa217607b45ff3e02a5917
Jonathan Kinred <jonathan.kinred@gmail.com>
2011-08-09 07:41:29 +1000

    Properly load configuration file and print message if one isn't found. Set default of default parameter to None.

--- psphere/config.py
+++ psphere/config.py
@@ -2,9 +2,18 @@ import os
 import sys
 import yaml
 
-PSPHERE_CONFIG = yaml.load(file(os.path.expanduser('~/.psphere/config.yml'),
-                                "r"))
-def _config_value(section, name, default):
+config_path = os.path.expanduser('~/.psphere/config.yml')
+try:
+    config_file = open(config_path, "r")
+except IOError:
+    print("Configuration file could not be opened, perhaps you"
+          " haven't created one?")
+    pass
+
+PSPHERE_CONFIG = yaml.load(config_file)
+config_file.close()
+
+def _config_value(section, name, default=None):
     if name in PSPHERE_CONFIG[section]:
         file_value = PSPHERE_CONFIG[section][name]
 
