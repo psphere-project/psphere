0b12356b78b8f873ea2bb0b0a75c055bf2d45628
Jonathan Kinred <jonathan.kinred@gmail.com>
2011-08-09 17:46:50 +1000

    Check if config exists and print a warning message if it doesn't.

--- psphere/config.py
+++ psphere/config.py
@@ -1,25 +1,22 @@
 import os
-import sys
 import yaml
 
 config_path = os.path.expanduser('~/.psphere/config.yml')
 try:
     config_file = open(config_path, "r")
+    PSPHERE_CONFIG = yaml.load(config_file)
+    config_file.close()
 except IOError:
-    print("Configuration file could not be opened, perhaps you"
-          " haven't created one?")
+    print("WARNING: Configuration file %s could not be opened, perhaps you"
+          " haven't created one?" % config_path)
+    PSPHERE_CONFIG = {"general": {}, "logging": {}}
     pass
 
-PSPHERE_CONFIG = yaml.load(config_file)
-config_file.close()
 
 def _config_value(section, name, default=None):
+    file_value = None
     if name in PSPHERE_CONFIG[section]:
         file_value = PSPHERE_CONFIG[section][name]
-
-    if default is None and file_value is None:
-        print("You must set a %s" % name)
-        sys.exit(1)
 
     if file_value:
         return file_value
