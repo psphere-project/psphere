b8584ca10b3085569a686ecd373a08b35c42d783
Jonathan Kinred <jonathan.kinred@gmail.com>
2011-08-11 11:49:24 +1000

    Update the vmcreate example so that it reads templates

--- examples/vmcreate.py
+++ examples/vmcreate.py
@@ -13,207 +13,267 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import re
 import sys
 import time
-import yaml
 
+from psphere import template
 from psphere.client import Client
-from psphere.errors import ObjectNotFoundError
+from psphere.errors import TemplateNotFoundError
 from psphere.soap import VimFault
-from psphere.scripting import BaseScript
 
-class VMCreate(BaseScript):
-    def create_vm(self, name, compute_resource, datastore, disksize, nics,
-                  memory, num_cpus, guest_id, host=None):
-        # If the host is not set, use the ComputeResource as the target
-        if host is None:
-            target = self.client.find_entity_view("ComputeResource",
-                                          filter={"name": compute_resource})
-            resource_pool = target.resourcePool
-        else:
-            target = self.client.find_entity_view("HostSystem",
-                                                  filter={"name": host})
-            resource_pool = target.parent.resourcePool
+def create_vm(client, name, compute_resource, datastore, disksize, nics,
+              memory, num_cpus, guest_id, host=None):
+    """Create a virtual machine using the specified values.
 
-        # TODO: Convert the requested disk size to kb
-        disksize_kb = disksize * 1024 * 1024
+    :param name: The name of the VM to create.
+    :type name: str
+    :param compute_resource: The name of a ComputeResource in which to \
+            create the VM.
+    :type compute_resource: str
+    :param datastore: The name of the datastore on which to create the VM.
+    :type datastore: str
+    :param disksize: The size of the disk, specified in KB, MB or GB. e.g. \
+            20971520KB, 20480MB, 20GB.
+    :type disksize: str
+    :param nics: The NICs to create, specified in a list of dict's which \
+            contain a "network_name" and "type" key. e.g. \
+            {"network_name": "VM Network", "type": "VirtualE1000"}
+    :type nics: list of dict's
+    :param memory: The amount of memory for the VM. Specified in KB, MB or \
+            GB. e.g. 2097152KB, 2048MB, 2GB.
+    :type memory: str
+    :param num_cpus: The number of CPUs the VM will have.
+    :type num_cpus: int
+    :param guest_id: The vSphere string of the VM guest you are creating. \
+            The list of VMs can be found at \
+        http://www.vmware.com/support/developer/vc-sdk/visdk41pubs/ApiReference/index.html
+    :type guest_id: str
+    :param host: The name of the host (default: None), if you want to \
+            provision the VM on a \ specific host.
+    :type host: str
 
-        # A list of devices to be assigned to the VM
-        vm_devices = []
+    """
+    print("Creating VM %s" % name)
+    # If the host is not set, use the ComputeResource as the target
+    if host is None:
+        target = client.find_entity_view("ComputeResource",
+                                      filter={"name": compute_resource})
+        resource_pool = target.resourcePool
+    else:
+        target = client.find_entity_view("HostSystem", filter={"name": host})
+        resource_pool = target.parent.resourcePool
 
-        # Create a disk controller
-        controller = self.create_controller("VirtualLsiLogicController")
-        vm_devices.append(controller)
+    disksize_pattern = re.compile("^\d+[KMG]B")
+    if disksize_pattern.match(disksize) is None:
+        print("Disk size %s is invalid. Try \"12G\" or similar" % disksize)
+        sys.exit(1)
 
-        ds_to_use = None
-        for ds in target.datastore:
-            if ds.name == datastore:
-                ds_to_use = ds
-                break
+    if disksize.endswith("GB"):
+        disksize_kb = int(disksize[:-2]) * 1024 * 1024
+    elif disksize.endswith("MB"):
+        disksize_kb = int(disksize[:-2]) * 1024
+    elif disksize.endswith("KB"):
+        disksize_kb = int(disksize[:-2])
+    else:
+        print("Disk size %s is invalid. Try \"12G\" or similar" % disksize)
 
-        if ds_to_use is None:
-            print("Could not find datastore on %s with name %s" %
-                  (target.name, datastore))
-            sys.exit()
+    memory_pattern = re.compile("^\d+[KMG]B")
+    if memory_pattern.match(memory) is None:
+        print("Memory size %s is invalid. Try \"12G\" or similar" % memory)
+        sys.exit(1)
 
-        # Ensure the datastore is accessible and has enough space
-        if ds_to_use.summary.accessible is not True:
-            print("Datastore (%s) exists, but is not accessible" %
-                  ds_to_use.summary.name)
-            sys.exit()
-        if ds_to_use.summary.freeSpace < disksize_kb * 1024:
-            print("Datastore (%s) exists, but does not have sufficient"
-                  " free space." % ds_to_use.summary.name)
-            sys.exit()
+    if memory.endswith("GB"):
+        memory_mb = int(memory[:-2]) * 1024
+    elif memory.endswith("MB"):
+        memory_mb = int(memory[:-2])
+    elif memory.endswith("KB"):
+        memory_mb = int(memory[:-2]) / 1024
+    else:
+        print("Memory size %s is invalid. Try \"12G\" or similar" % memory)
 
-        disk = self.create_disk(datastore=ds_to_use, disksize_kb=disksize_kb)
-        vm_devices.append(disk)
-        
-        for nic in nics:
-            nic_spec = self.create_nic(target, nic)
-            if nic_spec is None:
-                print("Could not create spec for NIC")
-                sys.exit()
+    # A list of devices to be assigned to the VM
+    vm_devices = []
 
-            # Append the nic spec to the vm_devices list
-            vm_devices.append(nic_spec)
+    # Create a disk controller
+    controller = create_controller(client, "VirtualLsiLogicController")
+    vm_devices.append(controller)
 
-        vmfi = self.client.create("VirtualMachineFileInfo")
-        vmfi.vmPathName = "[%s]" % ds_to_use.summary.name
-        vm_config_spec = self.client.create("VirtualMachineConfigSpec")
-        vm_config_spec.name = name
-        vm_config_spec.memoryMB = memory
-        vm_config_spec.files = vmfi
-        vm_config_spec.annotation = "Auto-provisioned by psphere"
-        vm_config_spec.numCPUs = num_cpus
-        vm_config_spec.guestId = guest_id
-        vm_config_spec.deviceChange = vm_devices
+    ds_to_use = None
+    for ds in target.datastore:
+        if ds.name == datastore:
+            ds_to_use = ds
+            break
 
-        # Find the datacenter of the target
-        if target.__class__.__name__ == "HostSystem":
-            datacenter = target.parent.parent.parent
-        else:
-            datacenter = target.parent.parent
+    if ds_to_use is None:
+        print("Could not find datastore on %s with name %s" %
+              (target.name, datastore))
+        sys.exit(1)
 
+    # Ensure the datastore is accessible and has enough space
+    if ds_to_use.summary.accessible is not True:
+        print("Datastore (%s) exists, but is not accessible" %
+              ds_to_use.summary.name)
+        sys.exit(1)
+    if ds_to_use.summary.freeSpace < disksize_kb * 1024:
+        print("Datastore (%s) exists, but does not have sufficient"
+              " free space." % ds_to_use.summary.name)
+        sys.exit(1)
+
+    disk = create_disk(client, datastore=ds_to_use, disksize_kb=disksize_kb)
+    vm_devices.append(disk)
+    
+    for nic in nics:
+        nic_spec = create_nic(client, target, nic)
+        if nic_spec is None:
+            print("Could not create spec for NIC")
+            sys.exit(1)
+
+        # Append the nic spec to the vm_devices list
+        vm_devices.append(nic_spec)
+
+    vmfi = client.create("VirtualMachineFileInfo")
+    vmfi.vmPathName = "[%s]" % ds_to_use.summary.name
+    vm_config_spec = client.create("VirtualMachineConfigSpec")
+    vm_config_spec.name = name
+    vm_config_spec.memoryMB = memory_mb
+    vm_config_spec.files = vmfi
+    vm_config_spec.annotation = "Auto-provisioned by psphere"
+    vm_config_spec.numCPUs = num_cpus
+    vm_config_spec.guestId = guest_id
+    vm_config_spec.deviceChange = vm_devices
+
+    # Find the datacenter of the target
+    if target.__class__.__name__ == "HostSystem":
+        datacenter = target.parent.parent.parent
+    else:
+        datacenter = target.parent.parent
+
+    try:
+        task = datacenter.vmFolder.CreateVM_Task(config=vm_config_spec,
+                                                 pool=resource_pool._mo_ref)
+    except VimFault, e:
+        print("Failed to create %s: " % e)
+        sys.exit()
+
+    while task.info.state in ["queued", "running"]:
+        time.sleep(5)
+        task.update()
+        print("Waiting 5 more seconds for VM creation")
+
+    if task.info.state == "success":
+        elapsed_time = task.info.completeTime - task.info.startTime
+        print("Successfully created new VM %s. Server took %s seconds." %
+              (name, elapsed_time.seconds))
+    elif task.info.state == "error":
+        print("ERROR: The task for creating the VM has finished with"
+              " an error. If an error was reported it will follow.")
         try:
-            task = datacenter.vmFolder.CreateVM_Task(config=vm_config_spec,
-                                                     pool=resource_pool._mo_ref)
-        except VimFault, e:
-            print("Failed to create %s: " % e)
-            sys.exit()
+            print("ERROR: %s" % task.info.error.localizedMessage)
+        except AttributeError:
+            print("ERROR: There is no error message available.")
+    else:
+        print("UNKNOWN: The task reports an unknown state %s" %
+              task.info.state)
 
-        while task.info.state in ["queued", "running"]:
-            time.sleep(5)
-            task.update()
-            print("Waiting 5 more seconds for VM creation")
-
-        if task.info.state == "success":
-            elapsed_time = task.info.completeTime - task.info.startTime
-            print("Successfully created new VM %s. Server took %s seconds." %
-                  (name, elapsed_time.seconds))
-        elif task.info.state == "error":
-            print("ERROR: The task for creating the VM has finished with"
-                  " an error. If an error was reported it will follow.")
-            try:
-                print("ERROR: %s" % task.info.error.localizedMessage)
-            except AttributeError:
-                print("ERROR: There is no error message available.")
+def create_nic(client, target, nic):
+    """Return a NIC spec"""
+    # Iterate through the networks and look for one matching
+    # the requested name
+    for network in target.network:
+        if network.name == nic["network_name"]:
+            net = network
+            break
         else:
-            print("UNKNOWN: The task reports an unknown state %s" %
-                  task.info.state)
+            continue
 
-        # All done
-        self.client.logout()
+    # Success! Create a nic attached to this network
+    backing = client.create("VirtualEthernetCardNetworkBackingInfo")
+    backing.deviceName = nic["network_name"]
+    backing.network = net._mo_ref
 
-    def create_nic(self, target, nic):
-        """Return a NIC spec"""
-        # Iterate through the networks and look for one matching
-        # the requested name
-        for network in target.network:
-            if network.name == nic["network_name"]:
-                net = network
-                break
-            else:
-                return None
+    connect_info = client.create("VirtualDeviceConnectInfo")
+    connect_info.allowGuestControl = True
+    connect_info.connected = False
+    connect_info.startConnected = True
 
-        # Success! Create a nic attached to this network
-        backing = (self.client.
-               create("VirtualEthernetCardNetworkBackingInfo"))
-        backing.deviceName = nic["network_name"]
-        backing.network = net._mo_ref
+    new_nic = client.create(nic["type"]) 
+    new_nic.backing = backing
+    new_nic.key = 2
+    # TODO: Work out a way to automatically increment this
+    new_nic.unitNumber = 1
+    new_nic.addressType = "generated"
+    new_nic.connectable = connect_info
 
-        connect_info = self.client.create("VirtualDeviceConnectInfo")
-        connect_info.allowGuestControl = True
-        connect_info.connected = False
-        connect_info.startConnected = True
+    nic_spec = client.create("VirtualDeviceConfigSpec")
+    nic_spec.device = new_nic
+    nic_spec.fileOperation = None
+    operation = client.create("VirtualDeviceConfigSpecOperation")
+    nic_spec.operation = (operation.add)
 
-        new_nic = self.client.create(nic["type"]) 
-        new_nic.backing = backing
-        new_nic.key = 2
-        # TODO: Work out a way to automatically increment this
-        new_nic.unitNumber = 1
-        new_nic.addressType = "generated"
-        new_nic.connectable = connect_info
+    return nic_spec
 
-        nic_spec = self.client.create("VirtualDeviceConfigSpec")
-        nic_spec.device = new_nic
-        nic_spec.fileOperation = None
-        operation = (self.client.create("VirtualDeviceConfigSpecOperation"))
-        nic_spec.operation = (operation.add)
+def create_controller(client, controller_type):
+    controller = client.create(controller_type)
+    controller.key = 0
+    controller.device = [0]
+    controller.busNumber = 0,
+    controller.sharedBus = client.create("VirtualSCSISharing").noSharing
+    spec = client.create("VirtualDeviceConfigSpec")
+    spec.device = controller
+    spec.fileOperation = None
+    spec.operation = client.create("VirtualDeviceConfigSpecOperation").add
+    return spec
 
-        return nic_spec
+def create_disk(client, datastore, disksize_kb):
+    backing = client.create("VirtualDiskFlatVer2BackingInfo")
+    backing.datastore = None
+    backing.diskMode = "persistent"
+    backing.fileName = "[%s]" % datastore.summary.name
+    backing.thinProvisioned = True
 
-    def create_controller(self, controller_type):
-        controller = self.client.create(controller_type)
-        controller.key = 0
-        controller.device = [0]
-        controller.busNumber = 0,
-        controller.sharedBus = (self.client.
-                                create("VirtualSCSISharing").noSharing)
-        spec = self.client.create("VirtualDeviceConfigSpec")
-        spec.device = controller
-        spec.fileOperation = None
-        spec.operation = (self.client.
-                          create("VirtualDeviceConfigSpecOperation").add)
-        return spec
+    disk = client.create("VirtualDisk")
+    disk.backing = backing
+    disk.controllerKey = 0
+    disk.key = 0
+    disk.unitNumber = 0
+    disk.capacityInKB = disksize_kb
 
-    def create_disk(self, datastore, disksize_kb):
-        backing = self.client.create("VirtualDiskFlatVer2BackingInfo")
-        backing.datastore = None
-        backing.diskMode = "persistent"
-        backing.fileName = "[%s]" % datastore.summary.name
-        backing.thinProvisioned = True
+    disk_spec = client.create("VirtualDeviceConfigSpec")
+    disk_spec.device = disk
+    file_op = client.create("VirtualDeviceConfigSpecFileOperation")
+    disk_spec.fileOperation = file_op.create
+    operation = client.create("VirtualDeviceConfigSpecOperation")
+    disk_spec.operation = operation.add
 
-        disk = self.client.create("VirtualDisk")
-        disk.backing = backing
-        disk.controllerKey = 0
-        disk.key = 0
-        disk.unitNumber = 0
-        disk.capacityInKB = disksize_kb
+    return disk_spec
 
-        disk_spec = self.client.create("VirtualDeviceConfigSpec")
-        disk_spec.device = disk
-        file_op = self.client.create("VirtualDeviceConfigSpecFileOperation")
-        disk_spec.fileOperation = file_op.create
-        operation = self.client.create("VirtualDeviceConfigSpecOperation")
-        disk_spec.operation = operation.add
+def main(name, template_name):
+    """The main method for this script.
 
-        return disk_spec
+    :param name: The name of the VM to create.
+    :type name: str
+    :param template_name: The name of the template to use for creating \
+            the VM.
+    :type template_name: str
 
-def main():
-    #hosts = ["ssi4as1app01", "ssi4brmapp01", "ssi4chgapp01", "ssi4extapp01",
-    #         "ssi4oamapp01", "ssi4oamapp02", "ssi4ppcapp01", "ssi4sdfosb01",
-    #         "ssi4sdfosb02", "ssi4sdfosb03", "ssi4sdfstb01", "ssi4subapp01",
-    #         "ssi4upeapp01"]
+    """
     client = Client()
-    vmc = VMCreate(client)
-    vm_file = open("vm_info.yaml")
-    vm_info = yaml.safe_load(vm_file)
-    vm_file.close()
-    nic = {"network_name": "AE_SDFDIE VLAN", "type": "VirtualE1000"}
-    vmc.create_vm(vm_info["name"], vm_info["compute_resource"],
-                  vm_info["datastore"], vm_info["disksize"], vm_info["nics"],
-                  vm_info["memory"], vm_info["num_cpus"], vm_info["guest_id"])
+    try:
+        vm_template = template.load_template(template_name)
+    except TemplateNotFoundError:
+        vm_template = template.load_template("default")
+    except TemplateNotFoundError:
+        print("ERROR: You have not specified a template to use and do not"
+              " have a default template configured.")
+        sys.exit(1)
+
+    create_vm(client, name, vm_template["compute_resource"],
+              vm_template["datastore"], vm_template["disksize"],
+              vm_template["nics"], vm_template["memory"],
+              vm_template["num_cpus"], vm_template["guest_id"],
+              host=vm_template["host"])
+    client.logout()
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1], sys.argv[2])
