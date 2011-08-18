9ce4459c7a3154bc6697f1e2f2d068d25d4223f6
Jonathan Kinred <jonathan.kinred@gmail.com>
2011-08-18 22:48:00 +1000

    Install correctly

--- setup.py
+++ setup.py
@@ -7,18 +7,17 @@ from setuptools import setup
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-setup(name='psphere',
-      version='0.5.0',
-      description='vSphere SDK for Python',
-      long_description=read('README'),
-      author='Jonathan Kinred',
-      author_email='jonathan.kinred@gmail.com',
-      url='http://bitbucket.org/jkinred/psphere',
-      packages=['psphere'],
-      install_requires=['suds', 'PyYAML'],
-      keywords=['vsphere', 'vmware'],
-      classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "License :: OSI Approved :: Apache Software License",
-      ],
+setup(name="psphere",
+      version="0.5.0",
+      description="vSphere SDK for Python",
+      long_description=read("README"),
+      author="Jonathan Kinred",
+      author_email="jonathan.kinred@gmail.com",
+      url="http://bitbucket.org/jkinred/psphere",
+      packages=["psphere"],
+      package_data={"psphere": ["wsdl/*"]},
+      install_requires=["suds", "PyYAML"],
+      keywords=["vsphere", "vmware"],
+      classifiers=["Development Status :: 4 - Beta",
+                   "License :: OSI Approved :: Apache Software License"],
      )
