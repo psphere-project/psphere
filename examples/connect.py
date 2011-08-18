76e5d94e12717db685b0c0c66e893d7e4365a57b
Jonathan Kinred <jonathan.kinred@gmail.com>
2011-08-18 22:47:42 +1000

    Update the script to accept arguments

--- examples/connect.py
+++ examples/connect.py
@@ -13,20 +13,39 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+from psphere import config
 from psphere.client import Client
-from psphere.scripting import BaseScript
 
-class Connect(BaseScript):
-    def connect(self):
-        """A simple connection test to login and print the server time."""
-        print(self.client.si.CurrentTime())
+def main(options):
+    """A simple connection test to login and print the server time."""
+    server = config._config_value("general", "server", options.server)
+    if server is None:
+        raise ValueError("server must be supplied on command line"
+                         " or in configuration file.")
+    username = config._config_value("general", "username", options.username)
+    if username is None:
+        raise ValueError("username must be supplied on command line"
+                         " or in configuration file.")
+    password = config._config_value("general", "password", options.password)
+    if password is None:
+        raise ValueError("password must be supplied on command line"
+                         " or in configuration file.")
 
-def main():
-    client = Client()
+    client = Client(server=server, username=username, password=password)
     print('Successfully connected to %s' % client.server)
-    c = Connect(client)
-    c.connect()
+    print(client.si.CurrentTime())
     client.logout()
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    from optparse import OptionParser
+    usage = "Usage: %prog [options]"
+    parser = OptionParser(usage=usage)
+    parser.add_option("--server", dest="server",
+                      help="The server to connect to for provisioning")
+    parser.add_option("--username", dest="username",
+                      help="The username used to connect to the server")
+    parser.add_option("--password", dest="password",
+                      help="The password used to connect to the server")
+
+    (options, args) = parser.parse_args()
+    main(options)
