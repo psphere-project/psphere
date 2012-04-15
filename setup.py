d7c04767c4c531fedf043a79ef4fe38d0c319541
Jonathan Kinred <jonathan.kinred@gmail.com>
2012-04-15 12:44:45 +1000

    Update setup.py for new README.rst

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 setup(name="psphere",
       version="0.5.2",
       description="vSphere SDK for Python",
-      long_description=read("README"),
+      long_description=read("README.rst"),
       author="Jonathan Kinred",
       author_email="jonathan.kinred@gmail.com",
       url="http://bitbucket.org/jkinred/psphere",
