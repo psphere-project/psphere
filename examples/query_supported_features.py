2c06bc86168699fe2772da30d1243be321c1b770
Jonathan Kinred <jonathan.kinred@team.telstra.com>
2013-08-27 10:50:10 +1000

    Change license year

--- examples/query_supported_features.py
+++ examples/query_supported_features.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# Copyright 2010 Jonathan Kinred
+# Copyright 2013 Jonathan Kinred
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not
 # use this file except in compliance with the License. You may obtain a copy
